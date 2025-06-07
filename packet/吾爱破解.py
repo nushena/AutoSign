2a1e921a2c14e13744a200ed12b6785afcf1a0a9
nushena <1320281483@qq.com>
2025-06-07 22:36:31 +0800

    更新插件版本至202506071738，在插件中添加了登录保存cookie功能。

--- packet/吾爱破解.py
+++ packet/吾爱破解.py
@@ -1,3 +1,4 @@
+import time
 from seleniumbase import SB
 from nushen import Nushen
 from selenium.webdriver.common.by import By
@@ -50,6 +51,23 @@ def run():
         except Exception as e:
             nushen.dbPrint(pluginName,{str(e)})
             nushen.dbPrint(pluginName, "未能找到用户名元素，可能未登录或页面结构已改变",True)
+            loginRes=input("是否要登录 1继续 (等待8秒)")
+            for i in range(9):
+                time.sleep(1)
+                if loginRes!='':break
+            if loginRes!=1:return
+            print('收到你的回复啦 快去登录 等待90秒')
+            for i in range(91):
+                time.sleep(1)
+                print(f'快去登录 还剩{90-i}秒')
+            print('正在保存cookie')
+            # 保存cookie
+            saveCookieRes = nushen.setCookies(pluginName, sb.get_cookies(), pluginUrl)
+            if saveCookieRes:
+                print("已保存cookie")
+
+            else:
+                print('保存cookie失败,未知问题')
             return
             
         # 任务逻辑
@@ -69,4 +87,4 @@ def run():
 
 def getVersion():
     # 你要想不更新就可以改成999999999999
-    return '202506071737'
+    return '202506071738'
